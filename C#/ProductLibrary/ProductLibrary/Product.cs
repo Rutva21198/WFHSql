e7c42b071509d063a6d6f7793172bb43bdde5b97
Rutva Gajera <rutwapatel.99@gmail.com>
2020-05-04 15:38:02 +0530

    C# practical add

---
+++ C#/ProductLibrary/ProductLibrary/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Product
+{
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException(
+                "Name cannot be null or empty string",
+                "Name");
+            }
+            name = value;
+        }
+    }
+}
